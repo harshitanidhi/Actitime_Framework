acac52c1be66b6c7385c3a14284f7dd0ca27e5db
HARSHITA NIDHI <HARSHITA NIDHI@LAPTOP-5LT1O1K6>
2021-10-19 15:32:36 +0530

    push

---
+++ CRM_Maven_Project/src/test/java/com/crm/contacttest/ContactTest.java
@@ -0,0 +1,18 @@
+package com.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("execute createContactTest");
+	}
+	@Test
+	public void editContactTest() {
+		System.out.println("execute editContactTest");
+	}
+	@Test
+	public void deleteContactTest() {
+		System.out.println("execute deleteContactTest");
+	}
+}
---
+++ CRM_Maven_Project/src/test/java/com/crm/orgTest/OrgTest.java
@@ -0,0 +1,14 @@
+package com.crm.orgTest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createContactTest");
+	}
+	@Test
+	public void editOrgTest() {
+		System.out.println("execute editOrgTest");
+	}
+}
